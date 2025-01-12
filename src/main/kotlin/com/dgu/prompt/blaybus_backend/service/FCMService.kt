9a79b1365eeef65a901a66c183647f4c52acb11a
김규리 <kimkyuree1@naver.com>
2025-01-13 00:40:54 +0900

    Feat: FCM 기능

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/service/FCMService.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/service/FCMService.kt
@@ -1,4 +1,20 @@
 package com.dgu.prompt.blaybus_backend.service
 
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import org.springframework.stereotype.Service
+
+@Service
 class FCMService {
+
+    fun sendNotification(fcmToken: String, title: String, body: String) {
+        val message = Message.builder()
+            .setToken(fcmToken)
+            .putData("title", title)
+            .putData("body", body)
+            .build()
+
+        FirebaseMessaging.getInstance().send(message)
+    }
 }
