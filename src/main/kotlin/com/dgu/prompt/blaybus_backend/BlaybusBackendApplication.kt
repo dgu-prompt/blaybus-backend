c67765fb72a74eb284ac6a0e0ecf95423ab20f3c
kgpill <glagudvlf821@gmail.com>
2025-01-12 08:39:27 +0000

    Feat: 환경 변수 지원을 위한 .env 파일 로드 및 데이터베이스 설정 변경

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/BlaybusBackendApplication.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/BlaybusBackendApplication.kt
@@ -1,5 +1,5 @@
 package com.dgu.prompt.blaybus_backend
-
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +7,16 @@ import org.springframework.boot.runApplication
 class BlaybusBackendApplication
 
 fun main(args: Array<String>) {
+	// .env 파일 로드
+    val dotenv = dotenv {
+        directory = "./" // 프로젝트 루트 디렉토리에서 로드
+        ignoreIfMissing = true // .env 파일이 없으면 무시
+    }
+
+    // 환경 변수를 시스템 속성에 설정
+    dotenv.entries().forEach { (key, value) ->
+        System.setProperty(key, value)
+    }
+
 	runApplication<BlaybusBackendApplication>(*args)
 }
