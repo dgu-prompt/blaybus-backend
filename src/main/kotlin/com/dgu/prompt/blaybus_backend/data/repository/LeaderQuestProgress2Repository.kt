01440de63b1f2cc573a6056d5ea9d4abcc38d3af
SK-YI <leesunkyeong0823@gmail.com>
2025-01-13 17:15:17 +0900

    Feat: 테스트용 Exp2, LeaderQuest2, LeaderQuestProgress2 추가

---
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/LeaderQuestProgress2Repository.kt
@@ -0,0 +1,8 @@
+package com.dgu.prompt.blaybus_backend.data.repository
+
+import com.dgu.prompt.blaybus_backend.data.entity.LeaderQuestProgress2
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface LeaderQuestProgress2Repository : JpaRepository<LeaderQuestProgress2, Int> {
+
+}
