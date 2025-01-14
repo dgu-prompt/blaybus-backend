5be3d413967e9e543c7c2fd2890315253cb91a6d
SK-YI <leesunkyeong0823@gmail.com>
2025-01-14 23:21:55 +0900

    Feat: jobQuest2 entity, repo 추가, 직무별 퀘스트 시트- JobQuest 동기화 구현

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/JobQuest2Repository.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/JobQuest2Repository.kt
@@ -1,4 +1,8 @@
 package com.dgu.prompt.blaybus_backend.data.repository
 
-interface JobQuest2Repository {
-}
+import com.dgu.prompt.blaybus_backend.data.entity.JobQuest2
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface JobQuest2Repository : JpaRepository<JobQuest2, Int>
