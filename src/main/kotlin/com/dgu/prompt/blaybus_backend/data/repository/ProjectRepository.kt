be980e02f0c548469d906f199843efe219e9b009
kgpill <glagudvlf821@gmail.com>
2025-01-15 19:38:40 +0000

    Feat: 프로젝트 관련 API 및 서비스 구현

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/ProjectRepository.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/ProjectRepository.kt
@@ -3,5 +3,6 @@ package com.dgu.prompt.blaybus_backend.data.repository
 import com.dgu.prompt.blaybus_backend.data.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProjectRepository : JpaRepository<Project, Int>
-
+interface ProjectRepository : JpaRepository<Project, Int> {
+    fun findByEmployeeNumber(employeeNumber: Int): List<Project>
+}
