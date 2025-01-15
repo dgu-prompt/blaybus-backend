590711391b2207381baddab050c320672dd09c1d
SK-YI <leesunkyeong0823@gmail.com>
2025-01-16 04:33:18 +0900

    Fix: 프로젝트 테이블 수정

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/data/entity/Project.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/data/entity/Project.kt
@@ -12,8 +12,11 @@ data class Project(
     @Column(name = "project_id")
     val projectId: Int,
 
-    @Column(name = "employee_number", nullable = false)
-    val employeeNumber: Int,
+//    @Column(name = "employee_number", nullable = false)
+//    val employeeNumber: Int,
+    @ManyToOne
+    @JoinColumn(name = "employee_number", referencedColumnName = "employee_number", nullable = false)
+    val user2: Users2, // Foreign key reference to `Users`
 
     @Column(name = "project_name", nullable = false)
     val projectName: String,
