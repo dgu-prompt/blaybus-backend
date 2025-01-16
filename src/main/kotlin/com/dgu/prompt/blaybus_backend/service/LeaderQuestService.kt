4a009382531604d3fbb985e2cb11de29c250147e
SK-YI <leesunkyeong0823@gmail.com>
2025-01-16 20:07:09 +0900

    Fix: leaderQuest id 반환 추가

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/service/LeaderQuestService.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/service/LeaderQuestService.kt
@@ -54,6 +54,7 @@ class LeaderQuestService(
             }
 
             LeaderQuestResponse(
+                questId = leaderQuest.questId,
                 questTitle = leaderQuest.questTitle,
                 maxExpDo = leaderQuest.maxExpDo,
                 medianExpDo = leaderQuest.medianExpDo,
