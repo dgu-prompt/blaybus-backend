ae46eee8407682e7988f50f859e4ce3adce087be
SK-YI <leesunkyeong0823@gmail.com>
2025-01-16 14:41:10 +0900

    Fix: Fix: JobQuest2/Progress2 엔티티 삭제

--- src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/LeaderQuestRepository.kt
+++ src/main/kotlin/com/dgu/prompt/blaybus_backend/data/repository/LeaderQuestRepository.kt
@@ -3,9 +3,7 @@ package com.dgu.prompt.blaybus_backend.data.repository
 import com.dgu.prompt.blaybus_backend.data.entity.Departments
 import com.dgu.prompt.blaybus_backend.data.entity.LeaderQuest
 import com.dgu.prompt.blaybus_backend.data.entity.FrequencyType
-import com.dgu.prompt.blaybus_backend.data.entity.LeaderQuest2
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import java.util.*
 
